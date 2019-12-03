cfbd98ea1fe2935e68fae5aa0e90dac4f0542735
jiangbo <jiangbo@dist.com.cn>
2019-12-03 17:50:34 +0800

    "测试使用UDF"

--- learn-scala/src/main/scala/com/mintlolly/spark_hive/HiveConnection.scala
+++ learn-scala/src/main/scala/com/mintlolly/spark_hive/HiveConnection.scala
@@ -1,5 +1,11 @@
 package com.mintlolly.spark_hive
 
+import java.text.SimpleDateFormat
+import java.util.Date
+
+import com.mintlolly.utils.TimeUtils
+import org.apache.spark.sql.SparkSession
+
 /**
  *
  * @description
@@ -8,5 +14,19 @@ package com.mintlolly.spark_hive
  *
  */
 object HiveConnection {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .appName("HiveConnection")
+      .config("hive.metastore.uris","thrift://slave1:9083")
+      .config("spark.sql.warehouse.dir","hdfs://192.168.200.150:9000/warehouse/tablespace/managed/hive")
+      .enableHiveSupport()
+      .master("local[4]")
+      .getOrCreate()
 
+    spark.udf.register("toDate10",TimeUtils.toDate10 _)
+    spark.sql("show create table mobile_phone_signaling_data.bucket_signaling").show()
+    spark.sql("select msid, toDate10(signaling_time) from mobile_phone_signaling_data.bucket_signaling").show()
+
+  }
 }
