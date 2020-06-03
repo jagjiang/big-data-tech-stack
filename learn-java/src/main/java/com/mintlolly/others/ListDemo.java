2faee0c1258b6b82000658158f8029772e3647ea
jagjiang <mintlolly@outlook.com>
2020-06-03 10:29:15 +0800

    "循环链表"

--- learn-java/src/main/java/com/mintlolly/others/ListDemo.java
+++ learn-java/src/main/java/com/mintlolly/others/ListDemo.java
@@ -31,8 +31,32 @@ public class ListDemo {
         linkedList.addFirst("b");
         linkedList.forEach(System.out::println);
 
-        String e = "aaa";
-        Node<String> node = new Node<>(null, e, null);
-        System.out.println(node.next);
+       String[] params = new String[]{"aaa","bbb","ccc","ddd","eee"};
+
+
+        loopPrint(params);
+    }
+
+    /**
+     * 循环打印，链表，尾节点指向头部节点
+     * @param params
+     */
+    public static void loopPrint(String[] params){
+        Node<String> node = new Node<>(null, params[0], null);
+        node.prev = node;
+        Node<String> node1 = new Node<>(node,params[1],null);
+        node.next = node1;
+        for (int i = 2; i < params.length; i++) {
+            node1.next = new Node<>(node,params[i],null);
+            node1 = node1.next;
+            if(i == params.length - 1){
+                node1.next = node.prev;
+            }
+        }
+
+        do {
+            System.out.println(node.item);
+            node = node.next;
+        } while (node != null);
     }
 }
