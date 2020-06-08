c0abc3799be8fef02461c477fd5cd58dcf4d5080
jagjiang <mintlolly@outlook.com>
2020-06-08 16:05:03 +0800

    "循环链表"

--- learn-java/src/main/java/com/mintlolly/others/ListDemo.java
+++ learn-java/src/main/java/com/mintlolly/others/ListDemo.java
@@ -22,6 +22,7 @@ public class ListDemo {
             this.prev = prev;
         }
     }
+
     public static void main(String[] args) {
         List<String> arrayList = new ArrayList<>();
         arrayList.add("a");
@@ -31,25 +32,59 @@ public class ListDemo {
         linkedList.addFirst("b");
         linkedList.forEach(System.out::println);
 
-       String[] params = new String[]{"aaa","bbb","ccc","ddd","eee"};
+        String[] params = new String[]{"aaa", "bbb", "ccc", "ddd", "eee"};
 
 
-        loopPrint(params);
+//        loopPrint(params);
+//        selfLoop();
+        circularLoop(params);
     }
 
     /**
-     * 循环打印，链表，尾节点指向头部节点
-     * @param params
+     * 自身循环，头指针和尾指针指向自己就好
      */
-    public static void loopPrint(String[] params){
+    public static void selfLoop() {
+        Node<String> node = new Node<>(null, "aaa", null);
+        node.next = node;
+        node.prev = node;
+        do {
+            System.out.println(node.item);
+            node = node.prev;
+        } while (node != null);
+    }
+
+    /**
+     * 双向循环列表
+     */
+    public static void circularLoop(String[] params) {
         Node<String> node = new Node<>(null, params[0], null);
         node.prev = node;
-        Node<String> node1 = new Node<>(node,params[1],null);
+        for (int i = 1; i < params.length; i++) {
+            Node<String> node1 = new Node<>(node, params[i], null);
+            node.next = node1;
+            node = node.next;
+
+        }
+
+        do {
+            System.out.println(node.item);
+            node = node.next;
+        } while (node != null);
+    }
+    /**
+     * 循环打印，链表，尾节点指向头部节点
+     *
+     * @param params
+     */
+    public static void loopPrint(String[] params) {
+        Node<String> node = new Node<>(null, params[0], null);
+        node.prev = node;
+        Node<String> node1 = new Node<>(node, params[1], null);
         node.next = node1;
         for (int i = 2; i < params.length; i++) {
-            node1.next = new Node<>(node,params[i],null);
+            node1.next = new Node<>(node, params[i], null);
             node1 = node1.next;
-            if(i == params.length - 1){
+            if (i == params.length - 1) {
                 node1.next = node.prev;
             }
         }
@@ -59,4 +94,5 @@ public class ListDemo {
             node = node.next;
         } while (node != null);
     }
+
 }
