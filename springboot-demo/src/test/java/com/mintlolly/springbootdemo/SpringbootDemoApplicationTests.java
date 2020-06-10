d1954899b754780a193f5b89ec4f4ea1386c5406
jagjiang <mintlolly@outlook.com>
2020-06-10 16:45:04 +0800

    "添加springboot demo"

---
+++ springboot-demo/src/test/java/com/mintlolly/springbootdemo/SpringbootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mintlolly.springbootdemo;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
