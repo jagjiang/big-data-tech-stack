88a50d5e622787712e9f4f8342c17e74358c0400
jagjiang <mintlolly@outlook.com>
2020-06-17 11:47:40 +0800

    "java开始使用测试类Junit"

---
+++ learn-java/src/test/com/mintlolly/redis/RedisTest.java
@@ -0,0 +1,20 @@
+package com.mintlolly.redis;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import redis.clients.jedis.Jedis;
+
+/**
+ * Create by on jiangbo 2020/6/17 11:15
+ * <p>
+ * Description:
+ */
+public class RedisTest {
+    final static Logger LOG = LoggerFactory.getLogger(com.mintlolly.redis.RedisTest.class);
+    @Test
+    public void connTest(){
+        Jedis jedis = new Jedis("172.16.150.128", 6379);
+        LOG.info("连接成功 服务正在运行:{}", jedis.ping());
+    }
+}
