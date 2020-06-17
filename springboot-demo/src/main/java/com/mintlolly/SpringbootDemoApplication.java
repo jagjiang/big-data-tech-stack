17a147d641d73af40cbdbdcae47524ad96a61ece
jagjiang <mintlolly@outlook.com>
2020-06-17 11:50:24 +0800

    "添加springboot-demo"

--- springboot-demo/src/main/java/com/mintlolly/SpringbootDemoApplication.java
+++ springboot-demo/src/main/java/com/mintlolly/SpringbootDemoApplication.java
@@ -1,4 +1,4 @@
-package com.mintlolly.springbootdemo;
+package com.mintlolly;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
