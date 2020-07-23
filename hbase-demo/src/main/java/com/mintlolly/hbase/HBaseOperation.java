4209ab42d9c5cce55b165ca93c40d4dcb963d966
jagjiang <mintlolly@outlook.com>
2020-07-23 14:17:25 +0800

    "nothing"

--- hbase-demo/src/main/java/com/mintlolly/hbase/HBaseOperation.java
+++ hbase-demo/src/main/java/com/mintlolly/hbase/HBaseOperation.java
@@ -1,6 +1,7 @@
 package com.mintlolly.hbase;
 
 
+import com.google.common.collect.Sets;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.CompareOperator;
 import org.apache.hadoop.hbase.HBaseConfiguration;
@@ -11,10 +12,12 @@ import org.apache.hadoop.hbase.filter.BinaryComparator;
 import org.apache.hadoop.hbase.filter.Filter;
 import org.apache.hadoop.hbase.filter.RowFilter;
 import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.util.compaction.MajorCompactor;
 
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.Set;
 
 public class HBaseOperation {
 
@@ -149,6 +152,7 @@ public class HBaseOperation {
             scanner.close();
             table.close();
     }
+
     //关闭资源
     public static void close() throws IOException {
         conn.close();
---
+++ spark-demo/src/main/scala/com/mintlolly/source/ClientYarn.scala
@@ -0,0 +1,17 @@
+package com.mintlolly.source
+
+import org.apache.spark.deploy.SparkSubmit
+
+
+/**
+ * Create by on jiangbo 2020/7/20 14:07
+ *
+ * Description:
+ *
+ */
+class ClientYarn {
+  def main(args: Array[String]): Unit = {
+    SparkSubmit;
+
+  }
+}
