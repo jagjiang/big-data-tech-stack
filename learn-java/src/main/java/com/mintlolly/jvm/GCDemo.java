594c4e20f9075f977d9b453c175c03e2f1a7fee8
jagjiang <mintlolly@outlook.com>
2020-07-29 17:14:23 +0800

    "GC demo"

---
+++ learn-java/src/main/java/com/mintlolly/jvm/GCDemo.java
@@ -0,0 +1,12 @@
+package com.mintlolly.jvm;
+
+/**
+ * Create by on jiangbo 2020/7/28 17:49
+ * <p>
+ * Description: 查看JVM运行时多少内存是系统内存
+ */
+public class GCDemo {
+    public static void main(String[] args) {
+
+    }
+}
