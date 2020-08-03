0cf31ef586cbc6fa83ab741f1a8c4f9f49d78e65
jagjiang <mintlolly@outlook.com>
2020-08-03 13:23:57 +0800

    "geomesa demo"

--- spark-demo/src/main/scala/com/mintlolly/geomesa/Test.scala
+++ spark-demo/src/main/scala/com/mintlolly/geomesa/Test.scala
@@ -25,7 +25,7 @@ object Test {
 
   def main(args: Array[String]): Unit = {
 
-    Logger.getLogger("org").setLevel(Level.WARN)
+//    Logger.getLogger("org").setLevel(Level.WARN)
 
     val spark = SparkSession.builder().appName("GeomesaTest").master("local[3]")
       .config("LOGLEVEL","warn").getOrCreate()
@@ -49,17 +49,38 @@ object Test {
 //    val alteredDF = df.withColumn("point", st_makePoint($"Lon", $"Lat"))
 //    alteredDF.show
 
+    val conf = ConfigFactory.parseString(
+      """
+        | {
+        |   type         = "delimited-text",
+        |   format       = "CSV",
+        |   fields = [
+        |      { name = "phrase", transform = "concatenate($1, $2)" },
+        |      { name = "lat",    transform = "$3::double" },
+        |      { name = "lon",    transform = "$4::double" },
+        |      { name = "geom",   transform = "point($lon, $lat)" }
+        |      ]
+        | }
+        """.stripMargin)
 
-    val exampleConf = ConfigFactory.load("G:\\github_topic\\study\\spark-demo\\src\\main\\resources\\example.conf").root().render()
+    val exampleConf = ConfigFactory.parseResources("./example.conf").root().render()
 
     val params = Map(
       "geomesa.converter"        -> exampleConf,
       "geomesa.converter.inputs" -> "C:\\Users\\Ryanj\\Desktop\\example.csv",
-      "geomesa.sft"              -> "phrase:String,dtg:Date,geom:Point:srid=4326",
+      "geomesa.sft"              -> "phrase:String,geom:Point:srid=4326",
       "geomesa.sft.name"         -> "example")
 
-    val query = new Query("example")
-    val rdd = GeoMesaSpark(params).rdd(new Configuration(), sc, params, query)
-    rdd.foreach(println)
+    val dataFrame = spark.read
+      .format("geomesa")
+      .options(params)
+      .option("geomesa.feature", "example")
+      .load()
+
+    dataFrame.withColumn("buffer",st_bufferPoint($"geom",10)).show()
+
+//    val query = new Query("example")
+//    val rdd = GeoMesaSpark(params).rdd(new Configuration(), sc, params, query)
+
   }
 }
