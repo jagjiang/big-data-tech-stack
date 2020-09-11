e179d9b450bf1add660bd8c9da6e9c26ccd2be02
jagjiang <mintlolly@outlook.com>
2020-09-11 09:32:39 +0800

    "数据结构Demo"

---
+++ learn-java/src/main/java/com/mintlolly/others/TreeDemo.java
@@ -0,0 +1,16 @@
+package com.mintlolly.others;
+
+import java.util.TreeMap;
+
+/**
+ * Create by on jiangbo 2020/8/4 11:01
+ * <p>
+ * Description:
+ */
+public class TreeDemo {
+    public static void main(String[] args) {
+        TreeMap treeMap = new TreeMap();
+
+
+    }
+}
