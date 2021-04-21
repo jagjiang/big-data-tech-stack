4e262da6b27ac8f229a3abd8b791ba35261ce7e5
jiangbo <mintlolly@outlook.com>
2021-04-21 16:36:17 +0800

    动态规划

--- learn-java/src/main/java/com/mintlolly/leetcode/ClimbingStairs.java
+++ learn-java/src/main/java/com/mintlolly/leetcode/ClimbingStairs.java
@@ -7,23 +7,6 @@ package com.mintlolly.leetcode;
  * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
  * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
  * 注意：给定 n 是一个正整数。
- * <p>
- * ```java
- * public class Solution {
- * public int climbStairs(int n) {
- * if (n == 1) {
- * return 1;
- * }
- * int[] dp = new int[n + 1];
- * dp[1] = 1;
- * dp[2] = 2;
- * for (int i = 3; i <= n; i++) {
- * dp[i] = dp[i - 1] + dp[i - 2];
- * }
- * return dp[n];
- * }
- * }
- * ```
  */
 public class ClimbingStairs {
     /**
