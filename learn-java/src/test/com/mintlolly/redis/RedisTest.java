3af2033d102afdb2559bc406b001533d853a12f4
jiangbo <mintlolly@outlook.com>
2021-07-06 20:21:59 +0800

    redis

--- learn-java/src/test/com/mintlolly/redis/RedisTest.java
+++ learn-java/src/test/com/mintlolly/redis/RedisTest.java
@@ -14,7 +14,7 @@ public class RedisTest {
     final static Logger LOG = LoggerFactory.getLogger(com.mintlolly.redis.RedisTest.class);
     @Test
     public void connTest(){
-        Jedis jedis = new Jedis("172.16.150.128", 6379);
+        Jedis jedis = new Jedis("172.16.122.46", 6379);
         LOG.info("连接成功 服务正在运行:{}", jedis.ping());
     }
 }
