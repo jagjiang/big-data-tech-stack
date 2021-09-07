0eaf2a03af42be1f2c54238a8d79a3239e683904
jiangbo <mintlolly@outlook.com>
2021-09-07 11:14:05 +0800

    add offset

--- kafka-demo/src/main/java/com/mintlolly/consumer/ConsumerOperation.java
+++ kafka-demo/src/main/java/com/mintlolly/consumer/ConsumerOperation.java
@@ -4,6 +4,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -17,7 +18,7 @@ public class ConsumerOperation {
 
     final static Logger LOG = LoggerFactory.getLogger(ConsumerOperation.class);
 
-    public static KafkaConsumer<String, String> createConsumer(){
+    public static KafkaConsumer<String, String> createConsumer() {
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "consumer-test");
@@ -34,20 +35,20 @@ public class ConsumerOperation {
     /**
      * 单线程消费
      */
-    public static void consumerSingleThread(){
+    public static void consumerSingleThread() {
         KafkaConsumer<String, String> consumer = createConsumer();
         consumer.subscribe(Arrays.asList("input-topic"));
         final int minBatchSize = 10;
-        List<ConsumerRecord<String,String>> buffer = new ArrayList<>();
-        while (true){
-            ConsumerRecords<String,String> records = consumer.poll(Duration.ofSeconds(100));
+        List<ConsumerRecord<String, String>> buffer = new ArrayList<>();
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(100));
             for (ConsumerRecord<String, String> record : records) {
-                LOG.info("offset={},partition={},value={}",record.offset(),record.partition(),record.value());
+                LOG.info("offset={},partition={},value={}", record.offset(), record.partition(), record.value());
                 buffer.add(record);
             }
 
             //比如写数据库，批量的形式进行写入
-            if (buffer.size() >= minBatchSize){
+            if (buffer.size() >= minBatchSize) {
                 //如写入数据库的逻辑，下面只是打印
                 for (ConsumerRecord<String, String> stringStringConsumerRecord : buffer) {
                     System.out.println(stringStringConsumerRecord);
@@ -65,7 +66,7 @@ public class ConsumerOperation {
      * 独立消费者可以不设置group.id
      * 可以多线程消费，按指定分区进行消费
      */
-    public static void singleConsummerPattern(){
+    public static void singleConsummerPattern() {
         KafkaConsumer<String, String> consumer = createConsumer();
         KafkaConsumer<String, String> consumer1 = createConsumer();
 
@@ -82,22 +83,37 @@ public class ConsumerOperation {
 //        list.add(topicPartition2);
 //        consumer.assign(list);
 //        consumer1.assign(Arrays.asList(topicPartition3));
-        while (true){
+        while (true) {
 
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(100));
             for (ConsumerRecord<String, String> record : records) {
-                LOG.info("consumer: offset={},partition={},key={},value={}",record.offset(),record.partition(),record.key(),record.value());
+                LOG.info("consumer: offset={},partition={},key={},value={}", record.offset(), record.partition(), record.key(), record.value());
             }
 
             ConsumerRecords<String, String> records1 = consumer1.poll(Duration.ofSeconds(100));
             for (ConsumerRecord<String, String> record : records1) {
-                LOG.info("consumer1: offset={},partition={},key={},value={}",record.offset(),record.partition(),record.key(),record.value());
+                LOG.info("consumer1: offset={},partition={},key={},value={}", record.offset(), record.partition(), record.key(), record.value());
+            }
+        }
+    }
+
+    public static void fromBeginning() {
+        KafkaConsumer<String, String> consumer = createConsumer();
+        TopicPartition topicPartition = new TopicPartition("input-topic", 0);
+        consumer.assign(Arrays.asList(topicPartition));
+        consumer.seekToBeginning(Arrays.asList(topicPartition));
+
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofSeconds(100));
+            for (ConsumerRecord<String, String> record : records) {
+                LOG.info("consumer: offset={},partition={},key={},value={}", record.offset(), record.partition(), record.key(), record.value());
             }
         }
     }
 
     public static void main(String[] args) {
-        consumerSingleThread();
+//        consumerSingleThread();
 //        singleConsummerPattern();
+        fromBeginning();
     }
 }
