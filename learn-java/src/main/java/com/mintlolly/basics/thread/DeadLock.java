e8eb2f9572c71b86d4b5aa1dc9e92f2f00fa2149
jiangbo <mintlolly@outlook.com>
2021-11-18 14:29:33 +0800

    object.wait

--- learn-java/src/main/java/com/mintlolly/basics/thread/DeadLock.java
+++ learn-java/src/main/java/com/mintlolly/basics/thread/DeadLock.java
@@ -12,7 +12,10 @@ public class DeadLock {
     public void thread1() throws InterruptedException {
         synchronized(o1){
             System.out.println("线程1成功拿到1把锁");
-            Thread.sleep(100);
+            //wait 释放锁 Object方法
+//            o1.wait(1);
+            //sleep未释放锁Thread方法
+//            Thread.sleep(100);
             synchronized (o2){
                 System.out.println("线程1成功拿到两把锁");
             }
@@ -21,7 +24,7 @@ public class DeadLock {
     public void thread2() throws InterruptedException {
         synchronized(o2){
             System.out.println("线程2成功拿到1把锁");
-            Thread.sleep(500);
+//            Thread.sleep(500);
             synchronized (o1){
                 System.out.println("线程2成功拿到两把锁");
             }
@@ -54,6 +57,11 @@ public class DeadLock {
         });
 
         t1.start();
+        //当前线程要锁定该对象之后，才能用锁定的对象执行这些方法，
+        // 这里需要用到synchronized关键字，
+        // 锁定哪个对象就用哪个对象来执行notify(), notifyAll(),wait(), wait(long), wait(long, int)操作，
+        // 否则就会报IllegalMonitorStateException异常。
+//        t1.wait();
         t2.start();
 
 
