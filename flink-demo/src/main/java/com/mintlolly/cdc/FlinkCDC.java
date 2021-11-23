6bd581d40d137829a1057a760c7da3cc3416c036
jiangbo <mintlolly@outlook.com>
2021-11-23 13:42:27 +0800

    flink cdc

---
+++ flink-demo/src/main/java/com/mintlolly/cdc/FlinkCDC.java
@@ -0,0 +1,58 @@
+package com.mintlolly.cdc;
+
+import com.alibaba.ververica.cdc.connectors.mysql.MySQLSource;
+import com.alibaba.ververica.cdc.connectors.mysql.table.StartupOptions;
+import com.alibaba.ververica.cdc.debezium.DebeziumSourceFunction;
+import com.alibaba.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
+import org.apache.flink.api.common.restartstrategy.RestartStrategies;
+import org.apache.flink.runtime.state.filesystem.FsStateBackend;
+import org.apache.flink.streaming.api.CheckpointingMode;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.environment.CheckpointConfig;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * Created on 2021/11/22
+ *
+ * @author jiangbo
+ * Description:
+ */
+public class FlinkCDC {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        //开启CK
+        env.enableCheckpointing(5000);
+        //2.Flink-CDC 将读取 binlog 的位置信息以状态的方式保存在 CK,如果想要做到断点续传,需要从 Checkpoint 或者 Savepoint 启动程序
+        //2.1 开启 Checkpoint,每隔 5 秒钟做一次 CK
+        env.enableCheckpointing(5000L);
+        //2.2 指定 CK 的一致性语义
+        env.getCheckpointConfig().setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE);
+        //2.3 设置任务关闭的时候保留最后一次 CK 数据
+        env.getCheckpointConfig().enableExternalizedCheckpoints(CheckpointConfig.ExternalizedCheckpointCleanup.RETAIN_ON_CANCELLATION);
+        //2.4 指定从 CK 自动重启策略
+        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3, 2000L));
+        //2.5 设置状态后端
+        env.setStateBackend(new FsStateBackend("hdfs://master:8020/flinkCDC"));
+        //2.6 设置访问 HDFS 的用户名
+        System.setProperty("HADOOP_USER_NAME", "root");
+
+
+        DebeziumSourceFunction<String> mysqlSource = MySQLSource.<String>builder()
+                .hostname("master")
+                .port(3306)
+                .username("root")
+                .password("123456")
+                .databaseList("gmall")
+                //可选配置项,如果不指定该参数,则会读取上一个配置下的所有表的数据，注意：指定的时候需要使用"db.table"的方式
+                .tableList("gmall.user_info")
+                .startupOptions(StartupOptions.initial())
+                .deserializer(new StringDebeziumDeserializationSchema())
+                .build();
+
+        DataStreamSource<String> dss = env.addSource(mysqlSource);
+        dss.print();
+        env.execute();
+    }
+}
